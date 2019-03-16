06ac0ba2f54b472e350441e5c2a2ff011b331ac6
David Gallegos <david.gallegos@outlook.com>
2019-03-15 20:02:12 -0700

    suggetions

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+/* --- Var one for each gem ---
+
+--- each gem needs a random number var ----
+--- Computer needs a random number var ---
+--- 
+--- */
+var redGem = '';
+var blueGem = '';
+var yellowGem = '';
+var greenGem = '';
+var totalWins = '';
+var totalLoses = '';
+var computerRandom = '';
+var totalNumber = '0';
+//
+$("#blCrystal").on("click", function() {
+    alert("you click a crystal");
+});
+//Crystals activity
+
